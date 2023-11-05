a0cbadac58931eb8afadad5948ae3c13032390fd
RolyMorvern <141121305+RolyMorvern@users.noreply.github.com>
2023-11-05 13:57:41 +0100

    Update Loading.js

--- src/Loading.js
+++ src/Loading.js
@@ -1,13 +1,11 @@
 import "./Loading.css";
-import Typed from 'react-typed';
 import Animation from "./Images/coffee-loading-animation.gif";
 
 function Loading () {
 	return (
 		<div className="loading">
 			<h1 id="loading-header">
-				Loading
-				<Typed strings={["....."]} typeSpeed={500} />
+				Loading ...
 			</h1>
 			<img id="loading-img" src={Animation} alt="Loading Animation" />
 		</div>
