710202ef2b5d0c003d8950b82de8db6be818d0d1
RolyMorvern <141121305+RolyMorvern@users.noreply.github.com>
2023-11-05 14:13:24 +0100

    Update Loading.js

--- src/Loading.js
+++ src/Loading.js
@@ -1,15 +1,34 @@
+import React, { useEffect, useState } from "react";
 import "./Loading.css";
 import Animation from "./Images/coffee-loading-animation.gif";
 
-function Loading () {
-	return (
-		<div className="loading">
-			<h1 id="loading-header">
-				Loading ...
-			</h1>
-			<img id="loading-img" src={Animation} alt="Loading Animation" />
-		</div>
-	)
-};
+function Loading() {
+  const [dots, setDots] = useState("...");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDots(prevDots => {
+        if (prevDots === "...") {
+          return ".";
+        } else if (prevDots === ".") {
+          return "..";
+        } else {
+          return "...";
+        }
+      });
+    }, 750);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="loading">
+      <h1 id="loading-header">
+        Loading {dots}
+      </h1>
+      <img id="loading-img" src={Animation} alt="Loading Animation" />
+    </div>
+  );
+}
 
 export default Loading;
